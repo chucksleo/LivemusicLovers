bef32cef6f3d6988e1ced3f6c1eb912c2010f304
Chucks Ogwo <chucksogwo@gmail.com>
2021-05-21 12:56:37 +0100

    Initial Commit

---
+++ LivemusicLovers/LivemusicLovers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LivemusicLovers.MvcApplication" Language="C#" %>
---
+++ LivemusicLovers/LivemusicLovers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LivemusicLovers.Startup))]
+namespace LivemusicLovers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
