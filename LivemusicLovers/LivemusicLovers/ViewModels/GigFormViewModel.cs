0a33697be409c110de913038775c7459635a1ed6
Chucks Ogwo <chucksogwo@gmail.com>
2021-05-23 13:39:22 +0100

    Move DateTime calculation to GigFormViewModel

--- LivemusicLovers/LivemusicLovers/ViewModels/GigFormViewModel.cs
+++ LivemusicLovers/LivemusicLovers/ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using LivemusicLovers.Models;
+using System;
 using System.Collections.Generic;
 
 namespace LivemusicLovers.ViewModels
@@ -10,5 +11,13 @@ namespace LivemusicLovers.ViewModels
         public string Time { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+
+            }
+        }
     }
 }
